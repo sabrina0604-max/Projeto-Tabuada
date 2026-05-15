55d71bc595744cd52c724c1fc34c4615b14d967b
Sabrina Rodrigues Medeiros <sabrina17104@outlook.com>
2026-05-15 20:25:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+const multiplicationTable = document.querySelector("#multiplication-operations");
+
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++){
+        const result = number * i;
+
+        const template = `
+        <div class="row">
+        <div class="operation">${number} X ${i} = </div>
+        <div class="result">${result}</div>
+        </div>`;
+
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+        const row = htmlTemplate.querySelector(".row");
+        multiplicationTable.appendChild(row);
+    }
+}
+
+multiplicationForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber);
+
+})
